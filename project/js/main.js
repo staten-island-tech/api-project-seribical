ae8a6e57878ddc778158942c1df24adfcb551748
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-12-08 13:08:38 -0500

    aeds

--- project/js/main.js
+++ project/js/main.js
@@ -6,14 +6,17 @@ DOMSelectors.box.innerHTML = `
 `
 
 async function data(){
-  let anime= await fetch('https://api.jikan.moe/v4/anime');
-  let list=await anime.json();
-  console.log(list)
-  list.data.forEach((i)=>{
+  try{
+    let anime= await fetch('https://api.jikan.moe/v4/anime?q=quintessential');
+    let list=await anime.json();
+    console.log(list)
+    list.data.forEach((i)=>{
     DOMSelectors.box.insertAdjacentHTML(
       "afterbegin",
-      `<p>${i.title}</p>`
-    )
-  })
-}
+      `<p>${i.title_english}</p>`)
+    })}
+    catch{
+      console.log("check your gogddman code")
+    }}
+
 data();
