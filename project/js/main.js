d28316dd70cb6bbe8cf6c7deb77c940467a66f9b
Nathan Chen <nathaniel.chen.1990@gmail.com>
2023-12-11 12:29:44 -0500

    dofbbuodfiubeuierupfeviyvyevyfrvyofrugpf

--- project/js/main.js
+++ project/js/main.js
@@ -21,6 +21,16 @@ async function initial(){
         </div>
         `)
       })
+      DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div id="footer">
+          <button id="prev"><</button>
+          <input type="text" id="page" value="1">
+          <button id="next">></button>
+        </div>
+        `
+      )
   }
   catch{
     console.log("caught")
@@ -30,50 +40,77 @@ async function initial(){
 DOMSelectors.button.addEventListener("click", function(event){
   event.preventDefault;
   const info = document.querySelectorAll("#content")
+  const footer = document.querySelector("#footer")
+  footer.remove()
   info.forEach((i)=>i.remove())
   if (DOMSelectors.search.value===""){
     console.log('nad id win')
   }
   else{
-    data(DOMSelectors.search.value)
+    data(DOMSelectors.search.value, 1)
   }
   
 
 })
 
-async function data(name){
+async function data(name,page){
   try{
-
-    
-
-    let anime = await fetch(`https://api.jikan.moe/v4/anime?q=${name}&page=2`);
+    let anime = await fetch(`https://api.jikan.moe/v4/anime?q=${name}&page=${page}`);
     let list = await anime.json();
     let reverse = list.data.reverse();
     console.log(reverse)
     if(list.data.length==0){
       console.log('search better kid')
+      DOMSelectors.box.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <p id="content">no results for ${name}</p>
+        <div id="footer">
+          </div>
+        `
+      )
     }
-    else{
-      reverse.forEach((i)=>{
-        DOMSelectors.box.insertAdjacentHTML(
-          "afterbegin",
-          `
-          <div id=content>
-            <p>${i.title}</p>
-            <p>${i.rating}</p>
-          </div>
-          `)
-        })
-        DOMSelectors.box.insertAdjacentHTML(
-          "beforeend",
-          `
-          <div id="footer">
-            <button id="prev"><</button>
-            <input type="text" id="page">
-            <button id="next">></button>
-          </div>
-          `
-        )
+  else{
+    reverse.forEach((i)=>{
+      DOMSelectors.box.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <div id=content>
+          <p>${i.title}</p>
+          <p>${i.rating}</p>
+        </div>
+        `)
+      })
+      DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div id="footer">
+          <button class="pageselector" id="prev"><</button>
+          <input type="text" id="page" value="${page}">
+          <button class="pageselector" id="next">></button>
+        </div>
+        `)
+
+        function pageselect(){
+        const selectors = document.querySelectorAll(".pageselector")
+        selectors.forEach((i)=>{i.addEventListener("click",function(event){
+          event.preventDefault
+          const info = document.querySelectorAll("#content")
+          const footer = document.querySelector("#footer")
+          footer.remove()
+          info.forEach((i)=>i.remove())
+          if(i.textContent==">"){
+            const number = page+1
+            data(name,number)
+          }
+          if(i.textContent=="<"){
+            const number = page-1
+            data(name,number)
+          }
+
+          })
+        })}
+        pageselect();
       }
     }
   catch{
